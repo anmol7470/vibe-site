f73adfed3e88e1ed1461d79be8e775b24a6164a6
Anmol Hurkat <238388601+anmol7470@users.noreply.github.com>
2025-11-06 12:49:12 -0700

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ import { getUser } from '@/lib/auth/get-user'
 export default async function Home() {
   const user = await getUser()
 
-  return <AppContainer user={user} />
+  return <AppContainer user={user} serverProjectId="" />
 }
